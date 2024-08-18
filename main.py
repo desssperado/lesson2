8cc61605978821d27386fd75719ac60a845bfc0d
desssperado <gamzatgasanov0@gmail.com>
2024-08-19 01:13:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+total_tasks = 12
+print('total_task = 12')
+hours_spent = 1.5
+print('hours_spent = 1.5')
+course = 'python'
+print('average_execution_timr = 0.125')
+print(total_tasks / hours_spent)
+average_execution_time = 0.125
+print('Курс', course, 'Всего задач', total_tasks, 'Затрачено часов', hours_spent, 'Среднее время выполнения', average_execution_time,)
