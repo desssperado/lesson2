5cdcfe5cadaaad33d5daebc7f0ef3cda5938eede
desssperado <gamzatgasanov0@gmail.com>
2024-08-26 10:43:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print('total_task = 12')
 hours_spent = 1.5
 print('hours_spent = 1.5')
 course = 'python'
-print('average_execution_timr = 0.125')
 print(total_tasks / hours_spent)
+print(hours_spent / total_tasks)
 average_execution_time = 0.125
 print('Курс', course, 'Всего задач', total_tasks, 'Затрачено часов', hours_spent, 'Среднее время выполнения', average_execution_time,)
